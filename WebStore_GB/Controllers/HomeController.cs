a5e1b7933390b6501016c8732509f18afca54779
mxbooll <mxbooll@gmail.com>
2020-05-23 15:59:19 +0500

    Первое представление

--- WebStore_GB/Controllers/HomeController.cs
+++ WebStore_GB/Controllers/HomeController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore_GB.Models;
 
 namespace WebStore_GB.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Surname = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 50
+            },
+            new Employee
+            {
+                Id = 2,
+                Surname = "Петров",
+                FirstName = "Пётр",
+                Patronymic = "Петрович",
+                Age = 25
+            },
+            new Employee
+            {
+                Id = 3,
+                Surname = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 30
+            },
+        };
+
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Hello world";
+            return View(_employees);
         }
 
         public IActionResult Another()
