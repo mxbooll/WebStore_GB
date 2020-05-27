d4412efd4e8f4139744b48ab8f2df177309634da
mxbooll <mxbooll@gmail.com>
2020-05-27 19:18:22 +0500

    Заготовки для основных действий в контроллере Home

--- WebStore_GB/Controllers/HomeController.cs
+++ WebStore_GB/Controllers/HomeController.cs
@@ -5,5 +5,21 @@ namespace WebStore_GB.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+        
+        public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult CheckOut() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
     }
 }
