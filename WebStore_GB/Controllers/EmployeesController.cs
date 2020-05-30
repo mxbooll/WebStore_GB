b16c83b68d9fa2a63c1fd12dd5336725cddc865f
mxbooll <mxbooll@gmail.com>
2020-05-30 10:20:43 +0500

    Настройки представлений

--- WebStore_GB/Controllers/EmployeesController.cs
+++ WebStore_GB/Controllers/EmployeesController.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using WebStore_GB.Data;
 using WebStore_GB.Infrastructure.Interfaces;
 using WebStore_GB.Models;
 using WebStore_GB.ViewModels;
