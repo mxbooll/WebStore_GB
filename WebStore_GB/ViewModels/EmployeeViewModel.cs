5f99f125fe387c4be4ae6a86c9d61de1de563020
mxbooll <mxbooll@gmail.com>
2020-06-02 19:57:56 +0500

    Валидация модели с помощью атрибутов

--- WebStore_GB/ViewModels/EmployeeViewModel.cs
+++ WebStore_GB/ViewModels/EmployeeViewModel.cs
@@ -1,15 +1,30 @@
-﻿namespace WebStore_GB.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore_GB.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Имя является обязательным")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Длина имени должна быть от 3 до 200 символов")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата имени")]
         public string Name { get; set; }
 
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Фамилия является обязательной")]
+        [MinLength(3, ErrorMessage = "Длина фамилии должна быть больше 3 символов")]
         public string Surname { get; set; }
 
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
 
+        [Display(Name = "Возраст")]
+        [Required]
+        [Range(20, 80, ErrorMessage = "Возраст должен быть в пределах от 20 до 80 лет")]
         public int Age { get; set; }
     }
 }
