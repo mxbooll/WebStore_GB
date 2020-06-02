2fcf5c66540efdf2111742d3a891995dafeac3c0
mxbooll <mxbooll@gmail.com>
2020-06-02 20:29:52 +0500

    Определение секции и бренда

---
+++ WebStore_GB.Domain/Entities/Brand.cs
@@ -0,0 +1,10 @@
+﻿using WebStore_GB.Domain.Entities.Base;
+using WebStore_GB.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_GB.Domain.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
