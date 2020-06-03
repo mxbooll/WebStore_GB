fb8b9205ba9d7ba64a9873093db334ca6c6fa906
mxbooll <mxbooll@gmail.com>
2020-06-03 21:54:51 +0500

    Добавление контроллера каталога товаров

---
+++ WebStore_GB/Controllers/CatalogController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebStore_GB.Domain.Entities;
+using WebStore_GB.Infrastructure.Interfaces;
+using WebStore_GB.ViewModels;
+
+namespace WebStore_GB.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData _productData;
+
+        public CatalogController(IProductData productData) => _productData = productData;
+
+        public IActionResult Shop(int? SectionId, int? BrandId)
+        {
+            var filter = new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId
+            };
+
+            var products = _productData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).OrderBy(p => p.Order)
+            });
+        }
+    }
+}
