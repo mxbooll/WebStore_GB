cd5f89bf95e37b4fd035b1ab8ed52c500b8d49ff
mxbooll <mxbooll@gmail.com>
2020-06-08 19:17:45 +0500

    Доработка моделей для передачи их в БД

--- WebStore_GB.Domain/Entities/Base/BaseEntity.cs
+++ WebStore_GB.Domain/Entities/Base/BaseEntity.cs
@@ -1,9 +1,13 @@
-﻿using WebStore_GB.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore_GB.Domain.Entities.Base.Interfaces;
 
 namespace WebStore_GB.Domain.Entities.Base
 {
     public abstract class BaseEntity : IBaseEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
