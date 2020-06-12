2e8ab14966ce160778206723a11e33a3892a7cc4
mxbooll <mxbooll@gmail.com>
2020-06-12 09:36:52 +0500

    Заготовка сервиса для работы с сотрудниками в БД

---
+++ WebStore_GB/Infrastructure/Services/InSQL/SqlEmployeesData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using WebStore_GB.Infrastructure.Interfaces;
+using WebStore_GB.Models;
+using WevStore_GB.DAL.Context;
+
+namespace WebStore_GB.Infrastructure.Services.InSQL
+{
+    public class SqlEmployeesData : IEmployeesData
+    {
+        private readonly WebStoreDB _db;
+
+        public SqlEmployeesData(WebStoreDB db) => _db = db;
+
+        public IEnumerable<Employee> Get() { throw new System.NotImplementedException(); }
+
+        public Employee GetById(int id) { throw new System.NotImplementedException(); }
+
+        public int Add(Employee Employee) { throw new System.NotImplementedException(); }
+
+        public void Edit(Employee Employee) { throw new System.NotImplementedException(); }
+
+        public bool Delete(int id) { throw new System.NotImplementedException(); }
+
+        public void SaveChanges() { throw new System.NotImplementedException(); }
+    }
+}
