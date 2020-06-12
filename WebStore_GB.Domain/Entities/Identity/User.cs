4c3f6d1c90886e4537e1c375cbb7f19bb7801495
mxbooll <mxbooll@gmail.com>
2020-06-12 12:18:33 +0500

    Определяем что такое пользователь и что такое роль

---
+++ WebStore_GB.Domain/Entities/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore_GB.Domain.Entities.Identity
+{
+    public class User : IdentityRole
+    {
+        public const string ADMINISTRATOR = "Admin";
+        public const string DEFAULTADMINPASSWORD = "AdminPAssword";
+    }
+}
