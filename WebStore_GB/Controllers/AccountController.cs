92a1b00d11ab65c4f530ad96eb5d41bdd871a958
mxbooll <mxbooll@gmail.com>
2020-06-13 14:31:37 +0500

    Заготовка метода Login

--- WebStore_GB/Controllers/AccountController.cs
+++ WebStore_GB/Controllers/AccountController.cs
@@ -44,7 +44,32 @@ namespace WebStore_GB.Controllers
         }
         #endregion
 
-        public IActionResult Login() => View();
+        #region Login user
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var login_result = await _signInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+                false);
+
+            if (login_result.Succeeded)
+            {
+                if (Url.IsLocalUrl(Model.ReturnUrl))
+                    return Redirect(Model.ReturnUrl);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя, или пароль!");
+
+            return View(Model);
+        }
+        #endregion
 
         public IActionResult Logout() => View();
 
