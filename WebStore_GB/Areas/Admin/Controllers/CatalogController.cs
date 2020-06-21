d232e8bc84d9bd032afe8e14ed9f9ded4504d71d
mxbooll <mxbooll@gmail.com>
2020-06-21 10:04:42 +0500

    Попытка исправить макет области администратора

--- WebStore_GB/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore_GB/Areas/Admin/Controllers/CatalogController.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore_GB.Domain.Entities;
+using WebStore_GB.Infrastructure.Interfaces;
 
 namespace WebStore_GB.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CatalogController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
+
+        public IActionResult Index() => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id)
         {
-            return View();
+            var product = id is null
+                ? new Product()
+                : _ProductData.GetProductById((int)id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
         }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
 }
