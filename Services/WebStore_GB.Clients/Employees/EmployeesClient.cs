a1d256b7108041b55f1db2f569296c62e671badc
mxbooll <mxbooll@gmail.com>
2020-06-27 19:03:59 +0500

    Заготовка клиента для WebAPI управления сотрудниками

---
+++ Services/WebStore_GB.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using WebStore_GB.Clients.Base;
+using WebStore_GB.Domain;
+using WebStore_GB.Domain.Entities.Employees;
+using WebStore_GB.Interfaces.Services;
+
+namespace WebStore_GB.Clients.Employees
+{
+    public class EmployeesClient : BaseClient, IEmployeesData
+    {
+        public EmployeesClient(IConfiguration configuration) : base(configuration, WebApi.EMPLOYEES)
+        {
+        }
+
+        public int Add(Employee Employee)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Edit(Employee Employee)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
