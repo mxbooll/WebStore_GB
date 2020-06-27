e01a1500e6a7c11a549f994fe923553a56181058
mxbooll <mxbooll@gmail.com>
2020-06-27 19:23:51 +0500

    Доработка базового клиента WebAPI

--- Services/WebStore_GB.Clients/Base/BaseClient.cs
+++ Services/WebStore_GB.Clients/Base/BaseClient.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebStore_GB.Clients.Base
 {
@@ -23,5 +24,33 @@ namespace WebStore_GB.Clients.Base
                 }
             };
         }
+
+        public T Get<T>(string url) => GetAsync<T>(url).Result;
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var response = await _client.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+
+        public HttpResponseMessage Post<T>(string url, T item) => PostAsync<T>(url, item).Result;
+
+        public async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await _client.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        public HttpResponseMessage Put<T>(string url, T item) => PutAsync<T>(url, item).Result;
+
+        public async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await _client.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+
+        public HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+        public async Task<HttpResponseMessage> DeleteAsync(string url) => await _client.DeleteAsync(url);
     }
 }
