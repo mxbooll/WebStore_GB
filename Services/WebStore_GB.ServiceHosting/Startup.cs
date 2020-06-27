78c1401636437771281a6ad58c5577104e009de3
mxbooll <mxbooll@gmail.com>
2020-06-27 16:31:55 +0500

    Подключаем БД к WebStore.ServiceHosting

--- Services/WebStore_GB.ServiceHosting/Startup.cs
+++ Services/WebStore_GB.ServiceHosting/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore_GB.Interfaces.Services;
+using WebStore_GB.Services.Data;
+using WebStore_GB.Services.Products.InSQL;
+using WevStore_GB.DAL.Context;
 
 namespace WebStore_GB.ServiceHosting
 {
@@ -12,10 +17,19 @@ namespace WebStore_GB.ServiceHosting
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) => services.AddControllers();
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WebStoreDB>(opt =>
+               opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<WebStoreDBInitializer>();
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDBInitializer db)
+        {
+            db.Initialize();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
