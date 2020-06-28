a7e8f8e4dbe11fb2dc5b34e386ea9a58b5d46260
mxbooll <mxbooll@gmail.com>
2020-06-28 12:26:33 +0500

    Логика работы клиента управления сотрудниками

--- Services/WebStore_GB.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore_GB.Clients/Employees/EmployeesClient.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.Net.Http;
 using WebStore_GB.Clients.Base;
 using WebStore_GB.Domain;
 using WebStore_GB.Domain.Entities.Employees;
@@ -9,38 +10,18 @@ namespace WebStore_GB.Clients.Employees
 {
     public class EmployeesClient : BaseClient, IEmployeesData
     {
-        public EmployeesClient(IConfiguration configuration) : base(configuration, WebApi.EMPLOYEES)
-        {
-        }
+        public EmployeesClient(IConfiguration configuration) : base(configuration, WebApi.EMPLOYEES) { }
 
-        public int Add(Employee Employee)
-        {
-            throw new System.NotImplementedException();
-        }
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(_serviceAddress);
 
-        public bool Delete(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+        public Employee GetById(int id) => Get<Employee>($"{_serviceAddress}/{id}");
 
-        public void Edit(Employee Employee)
-        {
-            throw new System.NotImplementedException();
-        }
+        public int Add(Employee employee) => Post(_serviceAddress, employee).Content.ReadAsAsync<int>().Result;
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new System.NotImplementedException();
-        }
+        public void Edit(Employee employee) => Put(_serviceAddress, employee);
 
-        public Employee GetById(int id)
-        {
-            throw new System.NotImplementedException();
-        }
+        public bool Delete(int id) => Delete($"{_serviceAddress}/{id}").IsSuccessStatusCode;
 
-        public void SaveChanges()
-        {
-            throw new System.NotImplementedException();
-        }
+        public void SaveChanges() { }
     }
 }
