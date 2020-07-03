cb24da0cc90e4b7eb4f8feef54840e155d3ff9ff
mxbooll <mxbooll@gmail.com>
2020-07-03 12:15:39 +0500

    Возможности конфигуировния системы логирования

--- Services/WebStore_GB.ServiceHosting/Startup.cs
+++ Services/WebStore_GB.ServiceHosting/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.IO;
