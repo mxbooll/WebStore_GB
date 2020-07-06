a75acb71485ac7ef481cb20defb83fe34dc7c99b
mxbooll <mxbooll@gmail.com>
2020-07-06 13:24:09 +0500

    ЗАготовка промежуточного ПО обработки ошибок

---
+++ UI/WebStore_GB/Infrastructure/Midleware/ErrorHandlingMidleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebStore_GB.Infrastructure.Midleware
+{
+    public class ErrorHandlingMidleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMidleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
