5c005a1d9c28892db9677f93699d34711f0d21dc
mxbooll <mxbooll@gmail.com>
2020-07-06 11:41:20 +0500

    Система интеграции бибилотеки Log4NET в инфраструктуру логирования ASP.NET Core

---
+++ Common/WebStore_GB.Logger/Log4NetLoggerProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Xml;
+
+namespace WebStore_GB.Logger
+{
+    public class Log4NetLoggerProvider : ILoggerProvider
+    {
+        private readonly string _configurationFile;
+        private readonly ConcurrentDictionary<string, Log4NetLogger> _loggers = new ConcurrentDictionary<string, Log4NetLogger>();
+
+        public Log4NetLoggerProvider(string configurationFile) => _configurationFile = configurationFile;
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, category =>
+            {
+                var xml = new XmlDocument();
+                xml.Load(_configurationFile);
+                return new Log4NetLogger(category, xml["log4net"]);
+            });
+        }
+
+        public void Dispose() => _loggers.Clear();
+    }
+}
