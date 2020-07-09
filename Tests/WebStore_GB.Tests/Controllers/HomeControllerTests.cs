9d68e00295d9a5fbcf890dac27f28f1366c1afa1
mxbooll <mxbooll@gmail.com>
2020-07-09 09:09:29 +0500

    лишний using

--- Tests/WebStore_GB.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore_GB.Tests/Controllers/HomeControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using WebStore_GB.Controllers;
