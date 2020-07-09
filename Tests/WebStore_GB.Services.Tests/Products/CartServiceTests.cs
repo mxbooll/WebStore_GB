cf11abc8032a7ee9c991765ab6d518d58e156549
mxbooll <mxbooll@gmail.com>
2020-07-09 09:52:32 +0500

    Подготовка иходных данных для разных модульных тестов корзины

--- Tests/WebStore_GB.Services.Tests/Products/CartServiceTests.cs
+++ Tests/WebStore_GB.Services.Tests/Products/CartServiceTests.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Collections.Generic;
+using WebStore_GB.Domain.DTO.Products;
+using WebStore_GB.Domain.Entities;
 using WebStore_GB.Domain.Models;
 using WebStore_GB.Domain.ViewModels;
+using WebStore_GB.Interfaces.Services;
+using WebStore_GB.Services.Products;
 using Assert = Xunit.Assert;
 
 namespace WebStore_GB.Services.Tests.Products
@@ -9,6 +14,55 @@ namespace WebStore_GB.Services.Tests.Products
     [TestClass()]
     public class CartServiceTests
     {
+        private Cart _cart;
+
+        private Mock<IProductData> _productDataMock;
+        private Mock<ICartStore> _cartStoreMock;
+
+        private ICartService _cartService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, Quantity = 1 },
+                    new CartItem { ProductId = 2, Quantity = 3 }
+                }
+            };
+            _productDataMock = new Mock<IProductData>();
+            _productDataMock
+               .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns(new List<ProductDTO>
+                {
+                    new ProductDTO
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Price = 1.1m,
+                        Order = 0,
+                        ImageUrl = "Product1.png",
+                        Brand = new BrandDTO { Id = 1, Name = "Brand 1" },
+                        Section = new SectionDTO { Id = 1, Name = "Section 1"}
+                    },
+                    new ProductDTO
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Price = 2.2m,
+                        Order = 0,
+                        ImageUrl = "Product2.png",
+                        Brand = new BrandDTO { Id = 2, Name = "Brand 2" },
+                        Section = new SectionDTO { Id = 2, Name = "Section 2"}
+                    },
+                });
+            _cartStoreMock = new Mock<ICartStore>();
+            _cartStoreMock.Setup(c => c.Cart).Returns(_cart);
+            _cartService = new CartService(_productDataMock.Object, _cartStoreMock.Object);
+        }
+
         [TestMethod()]
         public void Cart_Class_ItemsCount_returns_Correct_Quantity()
         {
