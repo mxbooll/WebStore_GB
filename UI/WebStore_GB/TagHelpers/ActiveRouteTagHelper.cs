28ae19e75c5712d37202589b9da13cf92d4338f7
mxbooll <mxbooll@gmail.com>
2020-07-10 11:51:20 +0500

    Заготовка таг-хелпера подсветки активной ссылки

---
+++ UI/WebStore_GB/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Diagnostics;
+
+namespace WebStore_GB.TagHelpers
+{
+    [HtmlTargetElement(Attributes = ATTRIBUTE_NAME)]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        private const string ATTRIBUTE_NAME = "is-active-route";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(ATTRIBUTE_NAME);
+        }
+    }
+}
