0e6aa80f779645a1cc44a12c9e1f289eee89a024
mxbooll <mxbooll@gmail.com>
2020-07-13 10:06:02 +0500

    Захват параметров из атрибутов разметки html

--- UI/WebStore_GB/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStore_GB/TagHelpers/ActiveRouteTagHelper.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace WebStore_GB.TagHelpers
@@ -7,6 +12,18 @@ namespace WebStore_GB.TagHelpers
     public class ActiveRouteTagHelper : TagHelper
     {
         private const string ATTRIBUTE_NAME = "is-active-route";
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp all route data", DictionaryAttributePrefix = "asp route ")]
+        public IDictionary<string, string> RouteValues { get; set; } = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
