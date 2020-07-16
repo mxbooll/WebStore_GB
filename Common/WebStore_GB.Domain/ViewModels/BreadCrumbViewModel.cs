3368518ce88584f65c0af89de3a9289bbe4b18e9
mxbooll <mxbooll@gmail.com>
2020-07-16 19:47:02 +0500

    Представление визуального компонента отображения хлебных крошек

---
+++ Common/WebStore_GB.Domain/ViewModels/BreadCrumbViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebStore_GB.Domain.Entities;
+
+namespace WebStore_GB.Domain.ViewModels
+{
+    public class BreadCrumbViewModel
+    {
+        public Section Section { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public string Product { get; set; }
+    }
+}
